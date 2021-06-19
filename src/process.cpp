33ba268822995b087d0740520d1b6d5588a434c6
alan <alan_lee1994@hotmail.com>
2021-06-19 10:34:25 +0000

    update 6

--- src/process.cpp
+++ src/process.cpp
@@ -13,31 +13,31 @@ using std::to_string;
 using std::vector;
 using std::stol;
 
-Process::Process(int pid){Process::pid_ = pid;}
+Process::Process(int pid){Process::process_id = pid;}
 // TODO: Return this process's ID
-int Process::Pid() { return pid_; }
+int Process::Pid() { return process_id; }
 
 float Process::CpuUtilization() { 
-  long total_time=LinuxParser::ActiveJiffies(pid_);
-  long seconds=(LinuxParser::UpTime())-(LinuxParser::UpTime(pid_));
+  long total_time=LinuxParser::ActiveJiffies(process_id);
+  long seconds=(LinuxParser::UpTime())-(LinuxParser::UpTime(process_id));
   float cpu_usage = ((float)total_time/sysconf(_SC_CLK_TCK))/(float)seconds;
   return cpu_usage;
   
 }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return LinuxParser::Command(pid_); }
+string Process::Command() { return LinuxParser::Command(process_id); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return LinuxParser::Ram(pid_); }
+string Process::Ram() { return LinuxParser::Ram(process_id); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return LinuxParser::User(pid_); }
+string Process::User() { return LinuxParser::User(process_id); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
+long int Process::UpTime() { return LinuxParser::UpTime(process_id); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 bool Process::operator<(Process const& a) const { 
-  return stol(LinuxParser::Ram(pid_)) > stol(LinuxParser::Ram(a.pid_)); 
+  return stol(LinuxParser::Ram(process_id)) > stol(LinuxParser::Ram(a.process_id)); 
 }
