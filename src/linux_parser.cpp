9fcbabb2152d691aac1edb613e797d5ac2db7241
alan <alan_lee1994@hotmail.com>
2021-06-19 10:32:58 +0000

    update 6

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -261,13 +261,13 @@ string LinuxParser::Uid(int pid) {
 // REMOVE: [[maybe_unused]] once you define the function
 string LinuxParser::User(int pid) { 
   string uid = LinuxParser::Uid(pid);
-  string line, key, x, value;
+  string line, key, _ , value;
   std::ifstream stream(kPasswordPath);
   if (stream.is_open()) {
    while (std::getline(stream, line)) {
      std::replace(line.begin(), line.end(), ':', ' ');
      std::istringstream linestream(line);
-     linestream >> key >> x >> value;
+     linestream >> key >> _ >> value;
      if (value == uid) {
        return key;
      }
